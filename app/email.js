d6e738bba76e7b6971931bbfc00bca21ead6aaeb
Malik <snapfirecookie@gmail.com>
2022-05-02 23:01:56 +0500

    Tryna set up Ava test

--- app/email.js
+++ app/email.js
@@ -1,11 +1,13 @@
-import nodemailer from 'nodemailer';
+// .ENV & ROUTER
 import 'dotenv/config';
 import express from 'express';
-import Joi from 'joi';
 
 const app = express();
 
 app.listen(5000, () => console.log('Server is up and running on 5000'));
+
+// NODEMAILER API
+import nodemailer from 'nodemailer';
 
 const myEmail = 'arrow7829423abdumalik@gmail.com';
 const myPassword = '54sH5Q$$vzbh#sH9RWPx';
@@ -17,7 +19,11 @@ var transporter = nodemailer.createTransport({
     user: myEmail,
     pass: myPassword
   }
-}); 
+});
+
+
+// JOI VALIDATION
+import Joi from 'joi';
 
 const schema = Joi.object({
 
@@ -38,23 +44,81 @@ var mailOptions = {
   text: 'That was easy!'
 };
 
-app.post('/send', (req, res) => {
+// app.post('/send', (req, res) => {
     
-    try {
-        const value = schema.validateAsync(mailOptions);
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-            console.log(error);
-            } else {
-            console.log('Email sent: ' + info.response);
+//     try {
+//         const value = schema.validateAsync(mailOptions);
+//         transporter.sendMail(mailOptions, function(error, info){
+//             if (error) {
+//             console.log(error);
+//             } else {
+//             console.log('Email sent: ' + info.response);
+//             }
+//         });
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+    
+// });
+
+async function wrappedSendMail(mailOptions) {
+    return new Promise((resolve,reject)=>{
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+            user: myEmail,
+            pass: myPassword
             }
         });
-    }
-    catch (err) {
-        console.log(err);
-    }
-    
-});
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log("error is "+error);
+                resolve(false); // or use rejcet(false) but then you will have to handle errors
+            } else {
+                console.log('Email sent: ' + info.response);
+                resolve(true);
+            }
+        });
+    });
+};
+
+const sendmail = async(req) => {      
+    var mailOptions = {
+        from: myEmail,
+        to: sendEmail,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+    let resp = await wrappedSendMail(mailOptions);
+    console.log(resp);
+    return resp;
+} 
+
+sendmail();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// COURIER API
+
 
 // import 'dotenv/config';
 // import { CourierClient } from "@trycourier/courier";
--- app/test.js
+++ app/test.js
@@ -1,10 +1,68 @@
-import test from 'ava'; 
+import test from 'ava';
+import res from 'express/lib/response';
+import nodemailer from 'nodemailer';
+import SendmailTransport from 'nodemailer/lib/sendmail-transport';
 
-test('foo', t => {
-	t.pass();
+const myEmail = 'arrow7829423abdumalik@gmail.com';
+const myPassword = '54sH5Q$$vzbh#sH9RWPx';
+var sendEmail = 'm.usman.work@gmail.com';
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: myEmail,
+    pass: myPassword
+  }
 });
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
+async function wrappedSendMail(mailOptions) {
+    return new Promise((resolve,reject)=>{
+
+        let transporter = nodemailer.nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+            user: myEmail,
+            pass: myPassword
+            }
+        });
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log("error is "+error);
+                resolve(false); // or use reject(false) but then you will have to handle errors
+            } else {
+                console.log('Email sent: ' + info.response);
+                resolve(true);
+            }
+        });
+    });
+};
+
+sendmail = async(req) => {      
+    var mailOptions = {
+        from: myEmail,
+        to: sendEmail,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+    let resp = await wrappedSendMail(mailOptions);
+    console.log(resp);
+    return resp;
+} 
+
+test('first scenario', t => {
+	sendmail();
+	t.true(resp === true);
 });
+
+// test('sendEmail', mailOptions => {
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//             console.log(error);
+//             resolve(false);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//             resolve(true);
+//         }
+//     });
+// });
